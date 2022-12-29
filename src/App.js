4a0bcc224c150675aa058bc2f53cab9b6bf5837d
barakaganai <barakaganai01@gmail.com>
2022-12-29 21:47:31 +0300

    import js component into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Why from "./why/Why";
 import "./App.css";
+import Home from "./home/Home";
 import TopBar from "./topBar/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
+      <Why />
     </div>
   );
 }
