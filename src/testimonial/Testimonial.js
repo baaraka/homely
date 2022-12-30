d0bed5d8ab5ab19b91d4262b2afcd573447e90cc
barakaganai <barakaganai01@gmail.com>
2022-12-30 20:47:35 +0300

    create testimonial.js

---
+++ src/testimonial/Testimonial.js
@@ -0,0 +1,38 @@
+import "./Testimonial.css";
+
+export default function Testimonial() {
+  return (
+    <div className="test">
+      <div className="testHead">
+        <h1 className="testHeading">Testimonials</h1>
+        <p className="testParagraph">This is what our client are saying</p>
+      </div>
+      <div className="testBody">
+        <div className="testBodyList">
+          <h1 className="testBodyListItem">Paul James</h1>
+          <p className="testBodyParagraph">Lagos state</p>
+          <p className="testBodyParagraphs">
+            With a Homely Eats meal, you can serve delectable, home-cooked meals
+            to your family and friends in any location, anytime. anyday!
+          </p>
+        </div>
+        <div className="testBodyList">
+          <h1 className="testBodyListItem">Paul James</h1>
+          <p className="testBodyParagraph">Lagos state</p>
+          <p className="testBodyParagraphs">
+            With a Homely Eats meal, you can serve delectable, home-cooked meals
+            to your family and friends in any location, anytime. anyday!
+          </p>
+        </div>
+        <div className="testBodyList">
+          <h1 className="testBodyListItem">Paul James</h1>
+          <p className="testBodyParagraph">Lagos state</p>
+          <p className="testBodyParagraphs">
+            With a Homely Eats meal, you can serve delectable, home-cooked meals
+            to your family and friends in any location, anytime. anyday!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
