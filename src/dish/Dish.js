d782d5a9e988e3ccb0f99805a4d98822406d9d7b
barakaganai <barakaganai01@gmail.com>
2022-12-30 17:13:12 +0300

    create dish.js

---
+++ src/dish/Dish.js
@@ -0,0 +1,82 @@
+import "./Dish.css";
+
+export default function Dish() {
+  return (
+    <div className="dish">
+      <div className="dishHeading">
+        <h1 className="headingDish">Our daily dishes</h1>
+        <p className="paragraphDish">
+          Check out recommended dishes of your choice
+        </p>
+      </div>
+      <div className="dishBody">
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+        <div className="dishBodyDiv">
+          <img
+            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            alt=""
+            className="dishImg"
+          />
+          <p className="dishParagraphImg">Yam and egg sauce</p>
+          <span className="dishNumber">TZS5000.00/=</span>
+          <input className="dishInput" type="number" placeholder="1" />
+          <button className="dishButton">+ Add</button>
+        </div>
+      </div>
+    </div>
+  );
+}
