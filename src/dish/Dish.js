21572e4e38a8a2bbd2f3c9d0b0aa9a0919a94295
barakaganai <barakaganai01@gmail.com>
2022-12-31 21:47:48 +0300

    change image

--- src/dish/Dish.js
+++ src/dish/Dish.js
@@ -12,7 +12,7 @@ export default function Dish() {
       <div className="dishBody">
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://storage.dado.ng/wp-content/uploads/2019/06/06200804/Webp.net-resizeimage-3.png"
             alt=""
             className="dishImg"
           />
@@ -23,56 +23,56 @@ export default function Dish() {
         </div>
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://www.preciouscore.com/wp-content/uploads/2021/12/Jollof-Rice-With-Chicken-500x500.jpg"
             alt=""
             className="dishImg"
           />
-          <p className="dishParagraphImg">Yam and egg sauce</p>
-          <span className="dishNumber">TZS 5000.00/=</span>
+          <p className="dishParagraphImg">Jollof rice and chicken</p>
+          <span className="dishNumber">TZS 6000.00/=</span>
           <input className="dishInput" type="number" placeholder="1" />
           <button className="dishButton">+ Add</button>
         </div>
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://www.thepretendchef.com/wp-content/uploads/2017/11/nigerian-beans-porridge-1-7.jpg"
             alt=""
             className="dishImg"
           />
-          <p className="dishParagraphImg">Yam and egg sauce</p>
-          <span className="dishNumber">TZS 5000.00/=</span>
+          <p className="dishParagraphImg">Porridge beans</p>
+          <span className="dishNumber">TZS 2000.00/=</span>
           <input className="dishInput" type="number" placeholder="1" />
           <button className="dishButton">+ Add</button>
         </div>
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://ntachiosa.com/wp-content/uploads/2020/08/ntachi-2-1110x1110-eited2-.png"
             alt=""
             className="dishImg"
           />
-          <p className="dishParagraphImg">Yam and egg sauce</p>
-          <span className="dishNumber">TZS 5000.00/=</span>
+          <p className="dishParagraphImg">Semo and egusi soup</p>
+          <span className="dishNumber">TZS 4000.00/=</span>
           <input className="dishInput" type="number" placeholder="1" />
           <button className="dishButton">+ Add</button>
         </div>
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://pbs.twimg.com/media/Ey9RpckWQAIzcXj.jpg"
             alt=""
             className="dishImg"
           />
-          <p className="dishParagraphImg">Yam and egg sauce</p>
-          <span className="dishNumber">TZS 5000.00/=</span>
+          <p className="dishParagraphImg">Amala and ewedu soup</p>
+          <span className="dishNumber">TZS 8000.00/=</span>
           <input className="dishInput" type="number" placeholder="1" />
           <button className="dishButton">+ Add</button>
         </div>
         <div className="dishBodyDiv">
           <img
-            src="https://takethemameal.com/files_images_v2/stam.jpg"
+            src="https://allnigerianfoods.com/wp-content/uploads/okro_soup_recipe.jpg"
             alt=""
             className="dishImg"
           />
-          <p className="dishParagraphImg">Yam and egg sauce</p>
-          <span className="dishNumber">TZS 5000.00/=</span>
+          <p className="dishParagraphImg">Eba and okra soup</p>
+          <span className="dishNumber">TZS 3500.00/=</span>
           <input className="dishInput" type="number" placeholder="1" />
           <button className="dishButton">+ Add</button>
         </div>
