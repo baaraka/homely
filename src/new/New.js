0a613584ae0ad1d54999a1c6bfbcf4535fc7b648
barakaganai <barakaganai01@gmail.com>
2023-01-10 19:10:52 +0300

    update new.js

--- src/new/New.js
+++ src/new/New.js
@@ -1,32 +1,6 @@
-import firebase from "firebase/compat/app";
-import React, { useState } from "react";
-import db from "../firebase/Firebase";
 import "./New.css";
 
 export default function New() {
-  const [input, setInput] = useState("");
-  const [message, setMessage] = useState("");
-
-  const inputHandler = (e) => {
-    setInput(e.target.value);
-  };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (input) {
-      // console.log(input);
-      //add to firebase
-
-      db.collection("emails").add({
-        email: input,
-        time: firebase.firestore.fieldValue.serverTimestamp(),
-      });
-      setInput("");
-      setMessage("Thank you for subscribing!!..");
-      setTimeout(() => {
-        setMessage("");
-      }, 3000);
-    }
-  };
   return (
     <div className="new">
       <div className="newHead">
@@ -35,17 +9,14 @@ export default function New() {
           Enter your Email address to get daily offers and news
         </p>
         <div>
-          <form onSubmit={submitHandler}>
+          <form>
             <input
               className="newInput"
               type="email"
               placeholder="Enter Your Email"
-              onChange={inputHandler}
-              value={input}
             />
             <button className="newButton">Subscribe</button>
           </form>
-          {message && <alert>{message}</alert>}
         </div>
       </div>
     </div>
