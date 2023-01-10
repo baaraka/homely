129606178b5b0243a04a958c4b64ef23ea64f06d
barakaganai <barakaganai01@gmail.com>
2023-01-10 19:12:38 +0300

    add toggle menu div

--- src/topBar/TopBar.js
+++ src/topBar/TopBar.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./TopBar.css";
 
 export default function TopBar() {
+  const [show, setShow] = useState(false);
   return (
     <div className="topBar">
       <div className="topBarTitleLeft">
@@ -36,6 +38,42 @@ export default function TopBar() {
           </span>
         </button>
       </div>
+      <div className="toggleMenu">
+        <i
+          onClick={() => setShow(!show)}
+          className="showBtn fa-solid fa-bars"
+        ></i>
+      </div>
+      {show ? (
+        <div className="sideBarTitle">
+          <ul className="sideBarListItem">
+            <a className="aTop" href="#Home">
+              <li className="sideBarListItems">Home</li>
+            </a>
+            <a className="aTop" href="#WhyChooseUs">
+              <li className="sideBarListItems">Why Choose Us</li>
+            </a>
+            <a className="aTop" href="#OurDish">
+              <li className="sideBarListItems">Our dishes</li>
+            </a>
+            <a className="aTop" href="#AboutUs">
+              <li className="sideBarListItems">About Us</li>
+            </a>
+            <a className="aTop" href="#Testimonials">
+              <li className="sideBarListItems">Testimonials</li>
+            </a>
+          </ul>
+          <button className="sideBarCartItem">
+            <span className="topCart">
+              <i class="topBarCart fa-solid fa-cart-shopping"></i>
+            </span>
+            Cart
+            <span className="topInput">
+              <input className="topInputItem" type="total" />
+            </span>
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
